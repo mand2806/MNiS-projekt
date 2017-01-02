2a0786266607e6b4180c2315cfc2d977111fbbb0
Commit1 <mand2806@gmail.com>
2017-01-02 22:57:40 +0100

    do zrobienia

--- MNiS - symulacja grafu/KlasaGrafu.h
+++ MNiS - symulacja grafu/KlasaGrafu.h
@@ -3,41 +3,48 @@
 #define GRAPH_H_ 
 
 #include <vector>
+#include <list>
 
 const int MAX_S = 10; // maxymalna potêga s
 
-struct PostacOperatorowa {
-private:
+class PostacOperatorowa {
 	int licznik[MAX_S]; 
 	int mianownik[MAX_S];
 public:
+	PostacOperatorowa();
+	~PostacOperatorowa();
 	PostacOperatorowa mnozenie(PostacOperatorowa H);
 	PostacOperatorowa dodawanie(PostacOperatorowa H);
 };
 
-typedef std::vector < std::vector <int> > list;
-typedef std::vector < int >  connections;
-
-class Graph {
-private:
-	int V; // Liczba wierzcho³kow 
-	int E; // Liczba krawêdzi 
-	list adj; // Vector przechowuj¹cy pol¹cznia miedzy wierzcholkami (reprezentacja listowa) 
+class Edge 
+{
+	Node* wychodzacy;
+	Node* dochodzacy;
+	PostacOperatorowa transmitancja;
 public:
-	/*  Konstruktory */
-	Graph(); // Konstruktor tworz¹cy pusty graf 
-	~Graph(); // Destruktor 
-			  /*  Rozmiar */
-	bool empty(); // Zwraca true jesli graf jest pusty 
-	int size(); // Zwraca rozmiar grafu; 
-				/*  Modyfikacje */
-	void add_vertex(); // Dodaje pojednyczy wierzcholek do grafu 
-	void add_edge(int v, int w); // Dodaje krawedz pomiedzy wierzcholkami 
-								 /*  Wyswietlanie */
-	void write_out(); // Wypisuje wierzcholki wraz z ich polaczeniami 
-					  /*  Dodatkowe */
-	bool connection(int v, int w); // Zwraca true jesli istnieje bezposrednie polaczenie miedzy wierzcholkami 
+	Edge();
+	~Edge();
 };
+
+class Node
+{
+	list<Edge*> edges;
+	PostacOperatorowa value;
+public:
+	Node();
+	~Node();
+};
+
+class Graph
+{
+	vector<Edge> edges; // graf jest w³aœcicielem krawedzi
+	vector<Node> nodes; // i wierzcho³ków
+public:
+	Graph();
+	~Graph();
+};
+
 #endif
 
 
